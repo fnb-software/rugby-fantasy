17b4aeb5fb11d4b6c68ebadc7f64ba26053607d4
Flo <fbernard@fnb.company>
2023-09-25 22:32:04 +0200

    chore

--- minizinc/parseResult.js
+++ minizinc/parseResult.js
@@ -13,7 +13,7 @@ const parseResult = ({ teamIds, captainId }) => {
     console.log(
       `${i + 1}. ${p === captain ? '(c)' : ''} ${p.lastName} ${p.firstName} (${
         squads.find((s) => p.squadId === s.id)?.abbreviation
-      } - ${p.cost / 1000000}) - ${getPlayerScore(p) || 'N/A'} `
+      } - ${p.cost / 1000000}) - ${getPlayerScore(p) ?? 'N/A'} `
     );
   });
   console.log('');
