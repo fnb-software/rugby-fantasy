b45386cd318a01fec94ffea0e25d06ff643ab7d5
Flo <fbernard@fnb.company>
2023-09-25 22:16:11 +0200

    fix writeDzn

--- minizinc/writeDzn.js
+++ minizinc/writeDzn.js
@@ -1,12 +1,10 @@
 import fs from 'fs/promises';
 import getDzn from './getDzn';
-const file = './minizinc/fantasy-data.mzn';
-
-const MAX_PER_TEAM = 3;
+const file = './minizinc/fantasy.dzn';
 
 const writeDzn = async () => {
   await fs.rm(file);
   await fs.appendFile(file, getDzn());
 };
 
-export default writeDzn;
+writeDzn();
